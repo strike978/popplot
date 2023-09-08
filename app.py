27caf6146dbac2c3032d36a50b27f9dc572cde4d
Omar Nunez <onunez978@gmail.com>
2023-09-08 14:22:23 -0400

    removed plot button

--- app.py
+++ app.py
@@ -6,47 +6,42 @@ import plotly.figure_factory as ff
 
 # Setting the layout of the page to wide and the title of the page to G25 Dendrograms
 st.set_page_config(layout="wide", page_title="PopPlot", page_icon="🧬")
-st.header(':blue[Pop]Plot')
+st.header('Pop:green[Plot]')
 
-tab1, tab2 = st.tabs(["Data", "Plot"])
+# Creating a file uploader to upload data as CSV or text
+uploaded_file = st.file_uploader(
+    "Upload a CSV or text file", type=["csv", "txt"])
 
+# Reading the data from the file pops.csv and displaying it in the text area.
+default_data = open("EUROPE.txt", "r").read()
+if uploaded_file is not None:
+    default_data = uploaded_file.getvalue().decode('utf-8')
+data_input = st.text_area("Enter data in CSV format", value=default_data)
 
-with tab1:
-    # Creating a file uploader to upload data as CSV or text
-    uploaded_file = st.file_uploader(
-        "Upload a CSV or text file", type=["csv", "txt"])
+# Reading the data from the file pops.csv and displaying it in the text area.
+if data_input:
+    data = pd.read_csv(io.StringIO(data_input), header=None).iloc[:, 1:]
+    populations = pd.read_csv(io.StringIO(
+        data_input), header=None, usecols=[0])[0]
 
-    # Reading the data from the file pops.csv and displaying it in the text area.
-    default_data = open("EUROPE.txt", "r").read()
-    if uploaded_file is not None:
-        default_data = uploaded_file.getvalue().decode('utf-8')
-    data_input = st.text_area("Enter data in CSV format", value=default_data)
+# if st.button("Plot"):
+    with st.spinner("Creating Dendrogram..."):
+        labels = [i for i in populations]
+        height = max(20 * len(populations), 500)
+        fig = ff.create_dendrogram(
+            data,
+            orientation="right",
+            labels=labels,
+            linkagefun=lambda x: linkage(x, method="ward"),
+            # color_threshold=0.07,
+        )
+        fig.update_layout(
+            height=height,
+            yaxis={'side': 'right'}
+        )
+        fig.update_yaxes(
+            automargin=True,
+            range=[0, len(populations)*10]
+        )
 
-    # Reading the data from the file pops.csv and displaying it in the text area.
-    if data_input:
-        data = pd.read_csv(io.StringIO(data_input), header=None).iloc[:, 1:]
-        populations = pd.read_csv(io.StringIO(
-            data_input), header=None, usecols=[0])[0]
-
-with tab2:
-    if st.button("Plot"):
-        with st.spinner("Creating Dendrogram..."):
-            labels = [i for i in populations]
-            height = max(20 * len(populations), 500)
-            fig = ff.create_dendrogram(
-                data,
-                orientation="right",
-                labels=labels,
-                linkagefun=lambda x: linkage(x, method="ward"),
-                # color_threshold=0.07,
-            )
-            fig.update_layout(
-                height=height,
-                yaxis={'side': 'right'}
-            )
-            fig.update_yaxes(
-                automargin=True,
-                range=[0, len(populations)*10]
-            )
-
-            st.plotly_chart(fig, theme=None, use_container_width=True)
+        st.plotly_chart(fig, theme=None, use_container_width=True)
