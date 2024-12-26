dde3771e68ccdcb1e9fdb5b2469cf2de3f2510a4
strike978 <48102757+strike978@users.noreply.github.com>
2024-12-25 22:29:31 -0500

    Update from header to title

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if 'redo_history' not in st.session_state:
 
 # Setting the layout of the page to wide and the title of the page to PopPlot
 st.set_page_config(layout="wide", page_title="PopPlot", page_icon="🌎")
-st.header('Pop:green[Plot]')
+st.title('Pop:green[Plot] 🌎')
 
 # Define the available data files
 data_files = {
